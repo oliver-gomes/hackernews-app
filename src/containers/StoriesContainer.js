5ab3d4b248cd4fce10de69b4bedc0408e306c61c
Oliver Gomes <ogomesphoto@gmail.com>
2020-03-07 21:10:38 -0500

    single Story apply

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { getStoryIds } from "../services/hnApi";
+import { getStoryIds, getStory, storyUrl } from "../services/hnApi";
+import Story from "../components/Story";
 
 function StoriesContainer() {
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
-    getStoryIds().then(data => setStoryIds(data));
+    getStoryIds().then(data => setStoryIds(data.data));
   }, []);
-  return <p>{JSON.stringify(storyIds)}</p>;
+
+  return storyIds.map(storyId => <Story storyId={storyId}></Story>);
 }
 
 export default StoriesContainer;
