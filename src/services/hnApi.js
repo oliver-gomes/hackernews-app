1f4a0d22c0b0b5eacb74dbebfe82335abe5e32f2
Oliver Gomes <ogomesphoto@gmail.com>
2020-03-07 22:38:40 -0500

    deconstruct fix

--- src/services/hnApi.js
+++ src/services/hnApi.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { selectFields } from "../utils/selectField";
 
 export const baseUrl = `https://hacker-news.firebaseio.com/v0/`;
 
@@ -9,12 +10,12 @@ export const storyUrl = `${baseUrl}item/`;
 export const getStory = async storyId => {
   const result = await axios
     .get(`${storyUrl + storyId}.json`)
-    .then(data => data);
+    .then(({ data }) => data && selectFields(data));
 
   return result;
 };
 
 export const getStoryIds = async () => {
-  const result = await axios.get(newStoriesUrl).then(data => data);
+  const result = await axios.get(newStoriesUrl).then(({ data }) => data);
   return result;
 };
